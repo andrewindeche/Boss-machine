224b5b3826a47b41414eb010c7a73212ca0cb563
andrewindeche <indecheandrew@gmail.com>
2024-11-04 10:45:35 +0300

    add routes

--- server/api.js
+++ server/api.js
@@ -1,8 +1,46 @@
 const express = require('express');
 const apiRouter = express.Router();
+const { shallowEqual } = require('react-redux');
+const { getAllFromDatabase,
+    getFromDatabaseById, 
+    addToDatabase, 
+    updateInstanceInDatabase,
+    deleteAllFromDatabase, 
+    createMeeting, 
+    deleteFromDatabasebyId } = require('./db');
 
-apiRouter.get('/api',(req,res,next) => {
-    res.send('Welcome')
-})
+apiRouter.param('/api/minions',(req,res) => {
+    const minion = getFromDatabaseById('minion',id)
+    minion 
+        ? (req.minion = minion,next()) 
+        : res.status(404).send('Minion not found');
+});
 
+apiRouter.get('/',(req,res) => {
+    res.send(getAllFromDatabase(minions));
+});
+
+apiRouter.post('/',(req,res) => {
+    const newMinion = req.body;
+    res.send(addToDatabase(newMinion));
+});
+
+apiRouter.get('/minionId',(req,res) => {
+    res.send(req.minion);
+});
+
+apiRouter.put('/:minionId',(req,res) => {
+    const updatedMinion = req.body;
+    updatedMinion.id = req.minion.id;
+    const isUpdated = updateInstanceInDatabase('minions',updatedMinion);
+    isUpdated ? res.status(200).send(updatedMinion): res.status(500).send("not updated");
+});
+
+apiRouter.delete('/:minionId',(req,res) => {
+ const deleteMinion = deleteFromDatabasebyId('minion',req.minion.id);
+    deleteMinion 
+        ?(res.status(204).send("Deleted"))
+            :(res.status(500).send("Failed to delete"));
+
+});
 module.exports = apiRouter;
